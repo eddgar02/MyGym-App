1cafbff87cadb3088c03771f9dfe0775b7cc4c9b
edgarlarasanchez <edgar.lara7681@gmail.com>
2024-09-24 18:29:43 -0500

    Set up selection of workouts.
    Not yet functional, but got the layout figured out.

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -1,20 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import SectionWrapper from "./SectionWrapper";
+import { SCHEMES, WORKOUTS } from "../utils/swoldier";
+// import Button from "./Button";
 
 function Header(props) {
   const { index, title, description } = props;
   return (
-    <div>
-      <div>
-        <p>{index}</p>
-        <h4>{title}</h4>
+    <div className="flex flex-col gap-4">
+      <div className="flex items-center justify-center gap-2">
+        <p
+          className="text-3xl sm:text-4xl md:text-5xl 
+        font-semibold text-slate-400"
+        >
+          {index}
+        </p>
+        <h4 className="text-xl sm:text-2xl md:text-3xl">{title}</h4>
       </div>
-      <p>{description}</p>
+      <p className="text-sm sm:text-base mx-auto">{description}</p>
     </div>
   );
 }
 
 export default function Generator() {
+  let showModal = false;
+
+  function toggleModal() {
+    showModal = !showModal;
+  }
+
   return (
     <div className="min-h-screen ">
       <SectionWrapper
@@ -24,8 +37,35 @@ export default function Generator() {
         <Header
           index={"01"}
           title={"Pick your battle"}
-          description={"Select your workout to endure"}
+          description={"Select the workout you want to endure"}
         />
+        <div className="grid grid-cols-2 sm:grid-cols-4 gap-4">
+          {Object.keys(WORKOUTS).map((type, typeIndex) => {
+            return (
+              <button
+                className="bg-slate-950 border border-blue-400 duration-200 hover:border-blue-600 py-3 rounded-lg"
+                key={typeIndex}
+              >
+                <p className="capitalize">{type.replaceAll("_", " ")}</p>
+              </button>
+            );
+          })}
+        </div>
+        <Header
+          index={"02"}
+          title={"Main focus"}
+          description={"Select the muscles you want to work on"}
+        />
+        <div className="bg-slate-950 py-3 border border-solid border-blue-400 rounded-lg flex flex-col">
+          <button
+            onClick={toggleModal}
+            className="relative p-3 flex items-center justify-center"
+          >
+            <p>Select muscle groups</p>
+            <i class="fa-solid absolute right-3 top-1/2 -translate-y-1/2 fa-caret-down"></i>
+          </button>
+          {showModal && <div>modal</div>}
+        </div>
       </SectionWrapper>
     </div>
   );
--- src/components/SectionWrapper.jsx
+++ src/components/SectionWrapper.jsx
@@ -18,7 +18,9 @@ export default function SectionWrapper(props) {
           {title[2]}
         </h2>
       </div>
-      {children}
+      <div className="max-w-[800px] w-full flex flex-col mx-auto gap-10 p-4">
+        {children}
+      </div>
     </section>
   );
 }
