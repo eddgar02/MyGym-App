3663d4c93990ad87cbf65cdee88a8c2b00d49bb9
edgarlarasanchez <edgar.lara7681@gmail.com>
2024-09-23 19:57:36 -0500

    Main page completed.
    Introductory page with Tailwind styling complete with button added as well; but still no functionality.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,26 @@
 import React from "react";
 
 export default function Hero() {
-  return <div>Hero</div>;
+  return (
+    <div
+      className="min-h-screen flex flex-col gap-10
+    items-center justify-center text-center max-w-[700] 
+    w-full mx-auto"
+    >
+      <div className="flex flex-col gap-4">
+        <p>ALL IT TAKES IS</p>
+        <h1 className="font-semibold text-4xl sm:text-5xl md:text-6xl lg:text-7xl">
+          CONSISTENCY
+        </h1>
+      </div>
+      <p className="text-sm md:text-base font-light">
+        I acknowledge that in order to reach my fitness goals I will need to be
+        consistent. I need to be consistent in going to the gym and eating the
+        right foods.{" "}
+      </p>
+      <button className="px-8 py-4 rounded-md border-[2px] bg-slate-950 border-blue-400 border-solid blueShadow duration-200">
+        Accept & Continue
+      </button>
+    </div>
+  );
 }
